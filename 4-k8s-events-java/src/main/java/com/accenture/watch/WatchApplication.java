4e80e76e8bf5d91a30bfc713fc6a272e428f153c
Fabio Gomez <fabio.gomez.diaz@accenture.com>
2019-11-19 08:02:38 -0600

    - Adjusted demo 4

--- 4-k8s-events-java/src/main/java/com/accenture/watch/WatchApplication.java
+++ 4-k8s-events-java/src/main/java/com/accenture/watch/WatchApplication.java
@@ -9,7 +9,6 @@ import io.kubernetes.client.apis.ExtensionsV1beta1Api;
 import io.kubernetes.client.ApiClient;
 import io.kubernetes.client.ApiException;
 import io.kubernetes.client.Configuration;
-import io.kubernetes.client.apis.CoreV1Api;
 import io.kubernetes.client.models.V1Deployment;
 import io.kubernetes.client.util.Config;
 import io.kubernetes.client.util.Watch;
